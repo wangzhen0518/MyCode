d9916e18f877d9e647104fd371b5c87e09b98e43
汪震 <wangzhen0518@126.com>
2021-12-08 22:33:13 +0800

    2021/12/08

--- algorithm/DynamicProgramming/cut_rod.cpp
+++ algorithm/DynamicProgramming/cut_rod.cpp
@@ -1,51 +1,47 @@
 #include <iostream>
 
-#define MIN -10
-
-void cut_rod(int* p, int* r, int* s, int n) {
-    int max;
-    r[0] = 0;
-    s[0] = 0;
-    for (int i = 1; i <= n; i++) {
-        max = MIN;
-        for (int k = 1; k <= i; k++) {
-            if (max < p[k] + r[i - k]) {
-                max = p[k] + r[i - k];
-                s[i] = k;
-            }
-        }
-        r[i] = max;
-    }
+int cut_rod(int *value, int *r, int *s, int n) {
+  r[1] = value[1];
+  s[1] = 1;
+  s[0] = 0;
+  for (int i = 2; i <= n; i++) {
+    int max = value[i];
+    for (int j = 1; j <= i; j++)
+      if (value[i - j] + r[j] > max) {
+        max = value[i - j] + r[j];
+        s[i] = j;
+      }
+    r[i] = max;
+  }
+  return r[n];
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    // waste a[0]
-    int* p = new int[n + 1];
-    int* r = new int[n + 1];
-    int* s = new int[n + 1];
+  int n;
+  scanf("%d", &n);
+  int *value = new int[n + 1]{0};
+  int *r = new int[n + 1]{0};
+  int *s = new int[n + 1]{0};
+  for (int i = 1; i < n + 1; i++) scanf("%d", &value[i]);
 
-    for (int i = 1; i <= n; i++)
-        scanf("%d", &p[i]);
-    cut_rod(p, r, s, n);
-    printf("i");
-    for (int i = 0; i <= n; i++)
-        printf("%4d", i);
-    printf("\nr");
-    for (int i = 0; i <= n; i++)
-        printf("%4d", r[i]);
-    printf("\ns");
-    for (int i = 0; i <= n; i++)
-        printf("%4d", s[i]);
-    printf("\nsequence");
-    int i = n;
-    while (i != 0) {
-        printf("%3d ", s[i]);
-        i = i - s[i];
-    }
-    printf("\n");
-    delete[] s;
-    delete[] r;
-    delete[] p;
+  cut_rod(value, r, s, n);
+
+  printf("%4c", 'i');
+  for (int i = 0; i <= n; i++) printf("%4d", i);
+  printf("\n%4c", 'r');
+  for (int i = 0; i <= n; i++) printf("%4d", r[i]);
+  printf("\n%4c", 's');
+  for (int i = 0; i <= n; i++) printf("%4d", s[i]);
+
+  printf("\nmax value: ");
+  int i = n;
+  while (i != 0) {
+    printf("%4d", i - s[i]);
+    i = s[i];
+  }
+  printf("\n");
+  delete[] s;
+  delete[] r;
+  delete[] value;
+  return 0;
 }
